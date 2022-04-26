f9902bc55520988535b7d5f0e38c7d806a50c3cc
Kee Yu <keeeeeyu@gmail.com>
2022-04-26 16:43:39 -0700

    feeding mofel finished

--- dogcollector/main_app/models.py
+++ dogcollector/main_app/models.py
@@ -13,3 +13,22 @@ class Dog(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+class Feeding(models.Model):
+    date = models.DateField('Feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
