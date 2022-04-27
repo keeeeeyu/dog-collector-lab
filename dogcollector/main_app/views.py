e08cfa7aaae4ed760df265e5b45b221bca1b099d
Kee Yu <keeeeeyu@gmail.com>
2022-04-27 00:20:06 -0700

    working on bonus

--- dogcollector/main_app/views.py
+++ dogcollector/main_app/views.py
@@ -1,9 +1,10 @@
 from dataclasses import fields
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # Create your views here.
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
+from .forms import FeedingForm
 
 class DogCreate(CreateView):
     model = Dog
@@ -29,4 +30,14 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
-    return render(request, 'dog/detail.html', {'dog': dog})
+    feeding_form = FeedingForm()
+    return render(request, 'dog/detail.html', {'dog': dog, 'feeding_form': feeding_form})
+
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.dog_id = dog_id
+        new_feeding.save()
+    return redirect('detail', dog_id=dog_id)
